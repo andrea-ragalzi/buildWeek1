701df4c457586be630fde3e63f24e1d48cc192dc
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-02-20 21:51:03 +0100

    now generate random quest an answ

--- assets/js/Question.js
+++ assets/js/Question.js
@@ -97,21 +97,56 @@ const questions = [
         incorrect_answers: ["Python", "C", "Jakarta"],
     },
 ];
-
+const QUESTIONS_FILE = 'assets/js/questions.json';
 const button1 = document.getElementById("0");
 const button2 = document.getElementById("1");
 const button3 = document.getElementById("2");
 const button4 = document.getElementById("3");
 
-function getRandomNumbers(length) {
-    if(!length) {
+var getRandomNumbers = (length) => {
+    /**
+* Genera un array di numeri casuali univoci da 0 a 'length' (incluso)
+* utilizzando un set.
+* @param {number} length - La lunghezza dell'array da generare.
+* @returns {number[]} Un array di numeri casuali univoci da 0 a 'length'
+(incluso).
+    */
+    if (!length) {
         return;
     }
     let set = new Set();
     while (set.size <= length) {
-      set.add(Math.floor(Math.random() * (length + 1)));
+        set.add(Math.floor(Math.random() * (length + 1)));
     }
-    return set;
-  }
+    return [...set];
+}
 
-console.log(getRandomNumbers(5));
+var getRandomQuestions = () => {
+    /**
+ * Genera un insieme casuale di domande e relative risposte utilizzando
+ * un array di oggetti 'questions'.
+ * Le risposte sono ordinate casualmente per ciascuna domanda e mostrate
+ * come testo su quattro bottoni.
+ * @param {HTMLElement} button1 - Il primo bottone sul quale visualizzare
+ * la prima risposta.
+ * @param {HTMLElement} button2 - Il secondo bottone sul quale visualizzare
+ * la seconda risposta.
+ * @param {HTMLElement} button3 - Il terzo bottone sul quale visualizzare
+ * la terza risposta.
+ * @param {HTMLElement} button4 - Il quarto bottone sul quale visualizzare
+ * la quarta risposta.
+ */
+    for (let question of questions) {
+        let randomNumbers =
+            getRandomNumbers(question.incorrect_answers.length);
+        let answers =
+            question.incorrect_answers.concat(question.correct_answer);
+        button1.innerText = answers[randomNumbers[0]];
+        button2.innerText = answers[randomNumbers[1]];
+        button3.innerText = answers[randomNumbers[2]];
+        button4.innerText = answers[randomNumbers[3]];
+    }
+}
+
+getRandomQuestions();
+
