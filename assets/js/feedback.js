068f8ae33968dd127b1997a006c788c599802edb
DekanisHani <dekanis@outlook.it>
2023-02-20 17:43:13 +0100

    feedback da sistemare dal secondo click in poi

---
+++ assets/js/feedback.js
@@ -0,0 +1,42 @@
+/*const starList = document.getElementById("starList");
+starList.addEventListener("mouseover");
+starList.addEventListener("mouseout")*/
+var colorStar = (maxIndex) => {
+  for (let i = 0; i <= maxIndex; i++) {
+    let star = starList[i];
+    star.style.filter = "grayscale(0%)";
+  }
+}
+var unColorStar = (maxIndex) => {
+  for (let i = 0; i <= maxIndex; i++) {
+    let star = starList[i];
+    star.style.filter = "grayscale(100%)";
+  }
+}
+var clickedStar = () => {
+  selectedValue=true;
+  if (selectedValue)
+}
+var selectedValue = false;
+const starList = document.querySelectorAll(".star");
+console.log(starList);
+
+
+for (let i = 0; i < starList.length; i++) {
+  let star = starList[i];
+  star.addEventListener("mouseover", function () {
+    console.log("Il mouse è sopra l'elemento!");
+    colorStar(i);
+  });
+
+  star.addEventListener("mouseout", function () {
+    if(!selectedValue){
+      console.log("Il mouse ha lasciato l'elemento!");
+      unColorStar(i);
+    }
+  });
+  star.addEventListener("click", function () {
+    console.log("mouse ha cliccato");
+    clickedStar();
+  })
+}
