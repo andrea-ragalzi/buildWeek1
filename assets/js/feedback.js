190ba5f4335af3bbc638946ec95fe98738bf239f
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-02-21 09:26:15 +0100

    removed print

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,6 +1,3 @@
-/*const starList = document.getElementById("starList");
-starList.addEventListener("mouseover");
-starList.addEventListener("mouseout")*/
 var colorStar = (maxIndex) => {
   for (let i = 0; i <= maxIndex; i++) {
     let star = starList[i];
@@ -15,30 +12,25 @@ var unColorStar = (minIndex) => {
 }
 var clickedStar = (indexStar) => {
   selectedValue = true;
-  unColorStar(indexStar+1, starList.length - 1);
+  unColorStar(indexStar + 1, starList.length - 1);
 }
 
 var selectedValue = false;
 const starList = document.querySelectorAll(".star");
-console.log(starList);
 
 
 for (let i = 0; i < starList.length; i++) {
   let star = starList[i];
   star.addEventListener("mouseover", function () {
-    console.log("Il mouse è sopra l'elemento!");
     colorStar(i);
   });
 
   star.addEventListener("mouseout", function () {
     if (!selectedValue) {
-      console.log("Il mouse ha lasciato l'elemento!");
       unColorStar(0);
     }
   });
   star.addEventListener("click", function () {
-    console.log("mouse ha cliccato");
     clickedStar(i);
   })
 }
-//ciao da Andrea
