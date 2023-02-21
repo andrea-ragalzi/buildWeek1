7c8fed401ca86771f37440bc349ed5793f2a93fa
mindthediv <gabriele98.contact@gmail.com>
2023-02-21 16:03:46 +0100

    Page Results (completa all'80%) 	modified:   assets/css/styleResults.css 	modified:   assets/js/resultsScript.js 	modified:   results.html

--- assets/js/resultsScript.js
+++ assets/js/resultsScript.js
@@ -1,19 +1,35 @@
 
 var rootEl = document.querySelector(':root');
 var rootStyle = getComputedStyle(rootEl);
-var donut = document.querySelector('.donut');
-var donutBefore = getComputedStyle(donut,'::before');
-var percentage = 33;
+var wrongPercentText = document.getElementById('wrongPercent');
+var correctPercentText = document.getElementById('correctPercent');
+var wrongTotText = document.getElementById('wrongTot');
+var correctTotText = document.getElementById('correctTot');
+var wrongPercent = 33.3;
+var correctPercent = 66.7;
+var wrongTot = '2/6';
+var correctTot = '4/6';
 
-function checkPercent() {
-    console.log(rootStyle.getPropertyValue('--percentage'));
+window.onload = () => { // Al caricamento della pagina avvia l'animazione del grafico
+    var pos = 100; //L'animazione è al contrario, il 100 è lo "0"
+    var id = setInterval(frame, 45);
+    function frame() {
+        if(pos != Math.floor(wrongPercent)){
+            pos = --pos;
+            rootEl.style.setProperty('--percentage', `${pos}`); 
+            console.log(pos); 
+        } else { 
+            clearInterval(id);
+        }
+    }
+    
+    console.log(pos);
+};
+
+writeResults = () => {  //Scrive le stringhe di output dei risultati
+    correctPercentText.textContent = `${correctPercent}%`;
+    wrongPercentText.textContent = `${wrongPercent}%`;
+    correctTotText.textContent = `${correctTot} questions`;
+    wrongTotText.textContent = `${wrongTot} questions`;
 }
-
-checkPercent();
-
-function changePercent() {
-   rootEl.style.setProperty('--percentage',`${percentage}`);
-}
-
-console.log(rootStyle);
-changePercent();
+writeResults();
