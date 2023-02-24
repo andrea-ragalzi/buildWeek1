da3a38502610f97918f2b658bc2e028c8d4cbf1b
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-02-24 12:53:53 +0100

    removed unused script in html

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -1,19 +1,18 @@
-function selezione(){
+function selezione() {
 
     var selectDaVerificare = document.getElementById("Difficulty");
     var indiceSelezionato = selectDaVerificare.selectedIndex;
 
-   
+
     var valoreSelezionato = selectDaVerificare.options[indiceSelezionato];
     var testoDentroLopzione = valoreSelezionato.text;
-    
+
     return testoDentroLopzione;
-}    
+}
 
 var button = document.getElementById('proceed');
 
-button.addEventListener('click', ()=>{
-     localStorage.setItem('difficolta', selezione());
-    console.log(selezione())
-    })
-    
+button.addEventListener('click', () => {
+    localStorage.setItem('difficolta', selezione());
+})
+
